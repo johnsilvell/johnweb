4f62ddb5cc5480726e2e42401aa62d082e4bda13
John Silvell (BN) <john.silvell@bonniernews.se>
2025-11-25 16:02:08 +0100

    Started building JavaScript functions

---
+++ script.js
@@ -0,0 +1,9 @@
+function welcome() {
+    console.log("The website has loaded successfully.");
+    alert("It seems like the website has loaded successfully. Welcome!");
+}
+
+function leave() {
+    console.log("The user is leaving the page.");
+    alert("You are now leaving this website. Press OK to continue.");
+}
